b355ff2543186a4081ab45e7267e1e3731de3226
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-25 23:02:01 +0300

    Food list

---
+++ app/models/food.rb
@@ -0,0 +1,6 @@
+class Food < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_25_180430) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_25_184842) do
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "Measurement_unit"
+    t.integer "price"
+    t.integer "quantity"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -24,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_25_180430) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
 end
