e1c4ceeefa4095179572e81805009640474b5ff7
felixDev22 <felixnzomo@gmail.com>
2023-04-27 02:21:02 +0300

    Update routes, add recipes, recipe_foods

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
        root to: "foods#index"
+       resources :recipe_foods
+       resources :recipes
        resources :foods, only: [:index, :new, :create, :destroy]
 end
