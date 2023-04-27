66157647f94a84fe022fb721d490b04755aec4f6
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-27 15:54:34 +0300

    netsted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-       root to: "foods#index"
-       resources :recipe_foods
-       resources :recipes
-       resources :foods, only: [:index, :new, :create, :destroy]
+  resources :foods, only: [:index, :new, :create, :destroy]
+  resources :recipes, only: [:index, :show, :destroy, :new, :create] do
+     resources :recipe_foods, only: [:new, :create, :destroy]
+     resources :general_shopping_list, only: [:index ,:show]
+  end
+
+  root "home#index"
 end
