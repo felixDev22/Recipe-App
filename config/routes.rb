172f885560ec11cd24455cd86e7b21a35cf2b0ce
felixDev22 <felixnzomo@gmail.com>
2023-04-28 12:01:12 +0300

    Update routes and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'public_recipes/index'
   devise_for :users
   resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes, only: [:index, :show, :destroy, :new, :create] do
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   end
 
   match 'recipes/:recipe_id' => 'recipes#toggle_public', as: :toggle_public, via: :patch
-  get 'public_recipes', to: 'recipes#public_recipes'
+  get 'public_recipes', to: 'public_recipes#index'
   post 'shopping_list/recipe_id=:recipe_id', to: 'recipes#generate_list', as: 'generate_shopping_list'
   get 'shopping_list/recipe_id=:recipe_id', to: 'recipes#generate', as: "shopping_list"
 
